716215b4cd4eed8df17397233c0627f043b535f8
Castro <dvasuta@gmail.com>
2018-03-18 23:35:09 +0200

    Check deploy

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ bot.set('storage', tableStorage);
 bot.dialog('/', function (session) {
     //session.send('You said => ' + session.message.text);
     session.send({
-        "text": session
+        "text": session.message
     });
 });
